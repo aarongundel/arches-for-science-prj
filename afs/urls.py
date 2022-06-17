b59fd1dfc53fa099ee73f8174d74f0535dc4772a
Aaron Gundel <aaron.gundel@gmail.com>
2022-06-17 15:09:26 -0600

    allows for psd layer extraction into a manifest

--- afs/urls.py
+++ afs/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import include, url
 from django.conf import settings
 from django.conf.urls.static import static
 from arches.app.views.plugin import PluginView
+from django.urls import path
+from afs.views.manifest_manager import ManifestManagerView
 from afs.views.physical_thing_search import PhysicalThingSearchView
 from afs.views.physical_things_in_set import PhysicalThingSetView
 from afs.views.update_resource_list import UpdateResourceListView
@@ -10,6 +12,7 @@ from afs.views.digital_resources_by_object_parts import DigitalResourcesByObject
 uuid_regex = settings.UUID_REGEX
 
 urlpatterns = [
+    url(r"^image-service-manager", ManifestManagerView.as_view(), name="manifest_manager"),
     url(r"^", include("arches.urls")),
     url(r"^physical-thing-search-results", PhysicalThingSearchView.as_view(), name="physical-thing-search-results"),
     url(r"^physical-things-in-set", PhysicalThingSetView.as_view(), name="physical_things_set"),
@@ -20,3 +23,6 @@ urlpatterns = [
     ),
     url(r"^updateresourcelist", UpdateResourceListView.as_view(), name="updateresourcelist"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# if settings.DEBUG:
+#     urlpatterns += [path("silk/", include("silk.urls", namespace="silk"))]
